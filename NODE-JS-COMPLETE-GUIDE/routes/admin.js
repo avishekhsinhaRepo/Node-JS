3d9e648f64b6bc69718bff5f6c4a0a075f7587ae
Avishekh Sinha <avishekh_sinha@optum.com>
2021-10-21 18:51:05 +0530

    Express Project - Adding Pug

---
+++ NODE-JS-COMPLETE-GUIDE/routes/admin.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+
+const router = express.Router();
+
+const rootDir = require('../util/path');
+
+const products =[];
+router.get('/add-product',(req,res,next)=>{
+   // res.sendFile(path.join(rootDir,"views","add-product.html"));
+   res.render('add-product',{'viewName':'addProduct'});
+});
+
+router.post('/add-product',(req,res,next)=>{
+   products.push({'title':req.body.title});
+   console.log(req.body);
+   res.redirect("/");
+});
+//module.exports= router;
+exports.routes = router;
+exports.products = products;
---
+++ NODE-JS-COMPLETE-GUIDE/routes/shop.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const router = express.Router();
+
+const rootDir = require('../util/path');
+
+const adminData = require('./admin');
+
+router.get('/',(req,res,next)=>{
+  console.log(adminData.products);
+  //res.sendFile(path.join(rootDir,"views","shop.html"));
+  const products = adminData.products;
+  res.render('shop',{prods:products,'docTitle':'Shop','viewName':'shop'});
+ });
+module.exports=router;
